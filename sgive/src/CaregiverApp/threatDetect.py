a0a9ba089667a3a00aca13cad81b3b6748c4e579
RYUseless <janfilipzacek@seznam.cz>
2023-10-25 22:22:32 +0200

    jedla uwu

--- sgive/src/CaregiverApp/threatDetect.py
+++ sgive/src/CaregiverApp/threatDetect.py
@@ -2,27 +2,22 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.metrics import confusion_matrix
 import humanize as hm
 import os
 import pickle
 
 
 class MLdetectionOfSusURL:
-    def __init__(self):
+    def __init__(self, pathToCSV):
+        self.pathToCSV = pathToCSV
         # phishing detection model
-        self.model = LogisticRegression(max_iter=1000)
-        # idk
-        srcPath = os.path.dirname(os.getcwd())
-        self.fullPathToCsv = os.path.join(srcPath, "PhisingSiteURL/phishing_site_urls.csv")
-        if os.path.isfile(self.fullPathToCsv):
-            self.machineLearning()
-            self.predictURL()
-        else:
-            return
+        self.model = LogisticRegression(max_iter=4000)
+        self.vectorizer = TfidfVectorizer()
+
 
     def machineLearning(self):
-        generic = pd.read_csv(self.fullPathToCsv)
+        generic = pd.read_csv(self.pathToCSV)
         generic = generic.rename(columns={'URL': 'url', 'Label': 'threat'})
         generic.threat = generic.threat.replace({'bad': 1, 'good': 0})
 
@@ -32,12 +27,9 @@ class MLdetectionOfSusURL:
         print(generic.threat.value_counts().apply(hm.metric), "\n")
 
         print('Norm Benign (0) URLs, Norm Phishing URLs (1)')
-        print(generic.threat.value_counts(normalize=True).round(2))
-        self.vectorizer = TfidfVectorizer()
+        print(generic.threat.value_counts(normalize=True).round(1))
         feature = self.vectorizer.fit_transform(generic.url)
-        feature_train, feature_test, threat_train, threat_test = train_test_split(feature,
-                                                                                  generic.threat,
-                                                                                  random_state=0)
+        feature_train, feature_test, threat_train, threat_test = train_test_split(feature, generic.threat, random_state=0)
         print(
             f'Train URLs {hm.metric(feature_train.shape[0])} ({int(round(threat_train.shape[0] / len(generic), 2) * 100)}%)\n')
 
@@ -63,25 +55,36 @@ class MLdetectionOfSusURL:
         print('\nConfusion matrix')
         print('[TN FP]')
         print('[FN TP]')
-        cm_generic
-        # saving?
-        pickle.dump(self.vectorizer, open("vectorizer", "wb"))
-        pickle.dump(self.model, open("model", "wb"))
+        #cm_generic  # what this?
+
+        # saving vectorizer and model
+        pickle.dump(self.vectorizer, open("ML-saved/vectorizer", "wb"))
+        pickle.dump(self.model, open("ML-saved/model", "wb"))
 
 
     def predictURL(self):
         print("-- detection --")
-        url = ["https://www.google.en/"]
-        vectorizer = pickle.load(open("vectorizer", "rb"))
-        model = pickle.load(open("model", "rb"))
+        url = ["https://www.alza.cz/"]
+        vectorizer = pickle.load(open("ML-saved/vectorizer", "rb"))
+        model = pickle.load(open("ML-saved/model", "rb"))
         feature = vectorizer.transform(url)
         predict = model.predict(feature)
-        print(predict)
-        print("throught predict")
-        featureThrg = self.vectorizer.transform(url)
-        predictThrg = self.model.predict(featureThrg)
-        print("through predict:",predictThrg)
+        if predict==1:
+            print(f"\n{url} is possible threat")
+        else:
+            print("\nwe chillin")
 
 
 if __name__ == '__main__':
-    MLdetectionOfSusURL()
+    srcPath = os.path.dirname(os.getcwd())
+    fullPathToCsv = os.path.join(srcPath, "PhisingSiteURL/phishing_site_urls.csv")
+    ML = MLdetectionOfSusURL(fullPathToCsv)
+    if os.path.isfile(fullPathToCsv):
+        inpt = input("Wanna train dataset again? [y/N]")
+        if inpt == "y" or inpt =="Y":
+            ML.machineLearning()
+            ML.predictURL()
+        else:
+            ML.predictURL()
+    else:
+        exit(404)
