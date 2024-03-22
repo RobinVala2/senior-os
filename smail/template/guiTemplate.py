b37a58092cd8e55519030b2636f9272f01c6be40
RobinVala2 <241124@vutbr.cz>
2024-03-22 21:32:53 +0100

    merging configuration files

--- smail/template/guiTemplate.py
+++ smail/template/guiTemplate.py
@@ -9,21 +9,21 @@ from screeninfo import get_monitors
 
 def getButtonConf():
     # collecting values for font and colors
-    bg = JS.jsonRed('colors_info', "buttons_unselected")
-    bg_active = JS.jsonRed('colors_info', "buttons_selected")
+    bg = JS.jsonRed('GUI_template', "buttons_unselected")
+    bg_active = JS.jsonRed('GUI_template', "buttons_selected")
     # end of collecting values for font and colors
     return bg, bg_active
 
 
 def resolutionMath():
      #TODO: do len(get_monitors()) when choosing which screen to get in caregiver app
-    _numOfScreen = JS.jsonRed_upd('GlobalConfiguration', "numOfScreen")
+    _numOfScreen = JS.jsonRed('GlobalConfiguration', "numOfScreen")
     # this is from screen_info imported get_monitors to get always the first screen
     _screenWidth = get_monitors()[_numOfScreen].width
     _screenHeight = get_monitors()[_numOfScreen].height
     # upper frame (MenuFrame)width and height
-    panelWidth = int(_screenWidth / JS.jsonRed_upd('GUI_template', "width_divisor"))
-    panelHeight = int(_screenHeight / JS.jsonRed_upd('GUI_template', "height_divisor"))
+    panelWidth = int(_screenWidth / JS.jsonRed('GUI_template', "width_divisor"))
+    panelHeight = int(_screenHeight / JS.jsonRed('GUI_template', "height_divisor"))
     # lower frame (ApplicationFrame) width and height
     app_width = _screenWidth - panelWidth
     app_height = _screenHeight - panelHeight
@@ -82,7 +82,7 @@ class _MenuFrameTemplate:
 class ApplicationFrameTemplate:
     def __init__(self, root: tkinter.Tk):
         self.exitButtonPokus = None
-        bg_color = JS.jsonRed('colors_info', "app_frame")
+        bg_color = JS.jsonRed('GUI_template', "app_frame")
         self.root = root
         self.app_frame_width = resolutionMath()[3]
         self.app_frame_height = resolutionMath()[4]
@@ -133,7 +133,7 @@ class menuButtonCRT:
         """
 
     def createOptArr(self):  # this reads values from conf.json and creates array based of length that was given
-        _numberOfValues = JS.jsonRed('buttons_info', "num_of_menu_buttons")  # get values from conf.json
+        _numberOfValues = JS.jsonRed('GUI_template', "num_of_menu_buttons")  # get values from conf.json
         _counter = 1
         while _counter <= _numberOfValues:
             self.option.append(_counter)
@@ -151,7 +151,7 @@ class menuButtonCRT:
         if self.id_of_menu < len(self.option):
             new_dict2 = self.optButtons2.button_dict
             for i in new_dict2:
-                new_dict2[i].pack(side=LEFT, padx=JS.jsonRed('buttons_info', "padx_value"))
+                new_dict2[i].pack(side=LEFT, padx=JS.red_main_config('GUI_template', "padx_value"))
             self.button_dict[self.id_of_menu].pack_forget()
             self.button_dict[self.id_of_menu + 1].pack(side=LEFT, expand=True, fill='both')
             self.id_of_menu += 1
@@ -166,20 +166,25 @@ class menuButtonCRT:
                 new_dict2[i].pack_forget()
             new_dict = self.optButtons1.button_dict  # show again the first opt buttons
             for i in new_dict:
-                new_dict[i].pack(side=LEFT, padx=JS.jsonRed('buttons_info', "padx_value"))
+                new_dict[i].pack(side=LEFT, padx=JS.jsonRed('GUI_template', "padx_value"))
 
     def createButtons(self):  # this def creates menu and back action button
         # collecting values for font and colors from def
         getValues = getButtonConf()
         bg = getValues[0]
         bg_active = getValues[1]
+        font_family = JS.jsonRed('GlobalConfiguration', "fontFamily")
+        font_size = JS.jsonRed('GlobalConfiguration', "fontSize")
+        font_thickness = JS.jsonRed('GlobalConfiguration', "fontThickness")
+        font_info = font_family + " " + str(font_size) + " " + font_thickness
         # end of collecting values for font and colors
+
         for i in self.option:
             self.button_dict[i] = Button(self.menuBar, text=f"MENU {i}", command=lambda: self.menuActionUp())
             # buttons configuration:
             self.button_dict[i]['activebackground'] = bg_active
             self.button_dict[i]['bg'] = bg
-            self.button_dict[i]['font'] = JS.jsonRed('font_info', "font")
+            self.button_dict[i]['font'] = font_info
             self.button_dict[i]['borderwidth'] = 2
             self.button_dict[i]['relief'] = 'solid'
         self.button_dict[1].pack(side=LEFT, expand=True, fill='both')  # FIRST MENU
@@ -197,7 +202,7 @@ class optionsButtonsCRT1:
         self.createOPT1Buttons()
 
     def createOptArr(self):  # this reads values from conf.json and creates array based of length that was given
-        _numberOfValues = JS.jsonRed('buttons_info', "num_of_opt_on_frame")
+        _numberOfValues = JS.jsonRed('GUI_template', "num_of_opt_on_frame")
         _counter = 1
         while _counter <= _numberOfValues:
             self.option.append(_counter)
@@ -208,8 +213,12 @@ class optionsButtonsCRT1:
         getValues = getButtonConf()
         bg = getValues[0]
         bg_active = getValues[1]
-        numOPT = JS.jsonRed('buttons_info', "num_of_opt_on_frame")
-        padxValue = JS.jsonRed('buttons_info', "padx_value")
+        numOPT = JS.jsonRed('GUI_template', "num_of_opt_on_frame")
+        padxValue = JS.jsonRed('GUI_template', "padx_value")
+        font_family = JS.jsonRed('GlobalConfiguration', "fontFamily")
+        font_size = JS.jsonRed('GlobalConfiguration', "fontSize")
+        font_thickness = JS.jsonRed('GlobalConfiguration', "fontThickness")
+        font_info = font_family + " " + str(font_size) + " " + font_thickness
         # end of collecting values for font and colors
         # -------------------------------------------
         for i in self.option:
@@ -226,7 +235,7 @@ class optionsButtonsCRT1:
             # 1/x of width - padX value from both sides * num of buttons ↓
             self.button_dict[i]['width'] = int(self.sixWidth - (2 * padxValue * numOPT))
             self.button_dict[i]['bg'] = bg
-            self.button_dict[i]['font'] = JS.jsonRed('font_info', "font")
+            self.button_dict[i]['font'] = font_info
             self.button_dict[i]['height'] = self.sixHeight
             self.button_dict[i]['borderwidth'] = 2
             self.button_dict[i]['relief'] = 'solid'
@@ -247,7 +256,7 @@ class optionsButtonsCRT2:
         self.createOPT2Buttons()
 
     def createOptArr(self):  # this reads values from conf.json and creates array based of length that was given
-        _numberOfValues = JS.jsonRed('buttons_info', "num_of_opt_on_frame")
+        _numberOfValues = JS.jsonRed('GUI_template', "num_of_opt_on_frame")
         _counter = 1
         while _counter <= _numberOfValues:
             self.option.append(_counter)
@@ -258,8 +267,12 @@ class optionsButtonsCRT2:
         getValues = getButtonConf()
         bg = getValues[0]
         bg_active = getValues[1]
-        numOPT = JS.jsonRed('buttons_info', "num_of_opt_on_frame")
-        padxValue = JS.jsonRed('buttons_info', "padx_value")
+        numOPT = JS.jsonRed('GUI_template', "num_of_opt_on_frame")
+        padxValue = JS.jsonRed('GUI_template', "padx_value")
+        font_family = JS.jsonRed('GlobalConfiguration', "fontFamily")
+        font_size = JS.jsonRed('GlobalConfiguration', "fontSize")
+        font_thickness = JS.jsonRed('GlobalConfiguration', "fontThickness")
+        font_info = font_family + " " + str(font_size) + " " + font_thickness
         # end of collecting values for font and colors
         # -------------------------------------------
         for i in self.option:
@@ -274,7 +287,7 @@ class optionsButtonsCRT2:
             # 1/x of width - padX value from both sides * num of buttons ↓
             self.button_dict[i]['width'] = int(self.sixWidth - (2 * padxValue * numOPT))
             self.button_dict[i]['bg'] = bg
-            self.button_dict[i]['font'] = JS.jsonRed('font_info', "font")
+            self.button_dict[i]['font'] = font_info
             self.button_dict[i]['height'] = self.sixHeight
             self.button_dict[i]['borderwidth'] = 2
             self.button_dict[i]['relief'] = 'solid'
